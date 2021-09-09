2746abd9b2ab8a51c4c59e3d9c75b08dd8743362
willberOdoo <odoo@ampd1.odoo.com>
2021-09-09 19:36:16 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,31 @@
+#-*-coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'sunmary': """
+    Academy app to manage Trainig""",
+    
+    'description':"""
+       Academy Module to mange Trainong:
+       - Couse 
+       - Sessions
+       - Attendes
+    """,
+    
+    'author': 'Odoo0',
+    
+    'website': 'htpps://www.odoo.com',
+    
+    'category':'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [ 
+    ],
+}
